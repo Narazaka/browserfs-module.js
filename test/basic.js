d42759ec00027e905e98750f03559de2b663657e
Narazaka <info@narazaka.net>
2016-08-03 23:47:47 +0900

    refactor test

--- test/basic.js
+++ test/basic.js
@@ -3,33 +3,23 @@ const fsBase = new BrowserFS.FileSystem.InMemory();
 BrowserFS.initialize(fsBase);
 const Module = require('../browserfs-module.js');
 
+const fs = BrowserFS.BFSRequire('fs');
+// for browserfsModule.Module
+fs.writeFileSync('/a.js' ,'module.exports = function() {return require("b")()};');
+fs.mkdirSync('/node_modules');
+fs.writeFileSync('/node_modules/b.js' ,'module.exports = function() {return "bb"};');
+// for require("fs")
+fs.writeFileSync('/fs_test.js' ,'var fs = require("fs"); module.exports = function() {return fs.readFileSync("./file", {encoding: "utf8"})};');
+fs.writeFileSync('/file' ,'file content');
+
 import assert from 'power-assert';
 
 describe('browserfsModule.Module', function() {
-  before(function() {
-    // BrowserFS.initialize(this.fs);
-    const fs = BrowserFS.BFSRequire('fs');
-    fs.writeFileSync('/a.js' ,'module.exports = function() {return require("b")()};');
-    fs.mkdirSync('/node_modules');
-    fs.writeFileSync('/node_modules/b.js' ,'module.exports = function() {return "bb"};');
-  });
-  lazy('fs', function() { return new BrowserFS.FileSystem.InMemory() });
   subject(function() { return Module._load("/a") });
-  context('_load', function() {
-    it('basic', function() { assert(this.subject() === "bb") });
-  });
+  it('works', function() { assert(this.subject() === "bb") });
 });
 
 describe('require("fs")', function() {
-  before(function() {
-    // BrowserFS.initialize(this.fs);
-    const fs = BrowserFS.BFSRequire('fs');
-    fs.writeFileSync('/c.js' ,'var fs = require("fs"); module.exports = function() {return fs.readFileSync("./file", {encoding: "utf8"})};');
-    fs.writeFileSync('/file' ,'file content');
-  });
-  lazy('fs', function() { return new BrowserFS.FileSystem.InMemory() });
-  subject(function() { return Module._load("/c") });
-  context('_load', function() {
-    it('basic', function() { assert(this.subject() === "file content") });
-  });
+  subject(function() { return Module._load("/fs_test") });
+  it('works', function() { assert(this.subject() === "file content") });
 });
